7266786d6d498f6500e575c6cef10dccd05feb6a
Denllar <denisgabbasov06.09.2004@gmail.com>
2025-07-27 01:41:40 +0500

    HashRouter в Router.jsx

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import Main from "../pages/Main";
 import MainLayout from "../layouts/MainLayout";
 import Shop from "../pages/Shop";
@@ -7,7 +7,7 @@ import Delivery from "../pages/Delivery";
 
 const Router = () => {
   return (
-    <BrowserRouter>
+    <HashRouter >
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Main />} />
@@ -17,7 +17,7 @@ const Router = () => {
         </Route>
         <Route path="*" element={<Navigate to={"/"} />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter >
   );
 };
 
